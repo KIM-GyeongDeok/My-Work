a53a24e6527c672d416ad01ab3ada5e3babca9b2
kimkunduk <kimkunduk@kut.ac.kr>
2019-12-30 19:38:29 +0900

    mhd파일 읽는 테스트용

---
+++ test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 30 19:23:35 2019
+
+@author: 김병철
+"""
+from medpy.io import load
+image_data, image_header = load('testdata\\0b231f58-2173-56fd-ac96-0ec0d142f09a.mhd')
